dd86f585931c261fb913f76c5168d2aa78be7096
rosell-dk <bjorn@rosell.dk>
2021-05-25 18:54:31 +0200

    added "and", "or" and "in-array" conditions

--- src/classes/ConditionEvaluator.js
+++ src/classes/ConditionEvaluator.js
@@ -10,16 +10,37 @@ export default class ConditionEvaluator {
         },
         'arg2': true
     }*/
+    //console.log('Evaluating condition',  conditionDef);
     if (!conditionDef['type']) {
       return conditionDef;
     }
     switch (conditionDef['type']) {
       case 'equals':
-      console.log('equals?');
         var arg1 = ConditionEvaluator.evaluate(conditionDef['arg1'], optionValues);
         var arg2 = ConditionEvaluator.evaluate(conditionDef['arg2'], optionValues);
         return (arg1 == arg2);
         break;
+
+      case 'in-array':
+        var val = ConditionEvaluator.evaluate(conditionDef['value'], optionValues);
+        var arr = ConditionEvaluator.evaluate(conditionDef['array'], optionValues);
+        return (arr.indexOf(val) > -1);
+
+      case 'and':
+        var arg1 = ConditionEvaluator.evaluate(conditionDef['arg1'], optionValues);
+        if (!arg1) {
+          return false;
+        }
+        var arg2 = ConditionEvaluator.evaluate(conditionDef['arg2'], optionValues);
+        return !!arg2;
+
+      case 'or':
+        var arg1 = ConditionEvaluator.evaluate(conditionDef['arg1'], optionValues);
+        if (arg1) {
+          return true;
+        }
+        var arg2 = ConditionEvaluator.evaluate(conditionDef['arg2'], optionValues);
+        return !!arg2;
 
       case 'option-value':
         var optionId = conditionDef['option-id'];
